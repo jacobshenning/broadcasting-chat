29417c62416a1bbef38dc4e4f958e5aee51da3f7
Jacob Henning <jacobshenning@gmail.com>
2020-03-06 13:49:55 -0800

    TLS Switch

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,7 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
+                'useTLS' => env('PUSHER_TLS'),
                 'host' => env('PUSHER_HOST'),
                 'port' => 6001,
                 'scheme' => 'http'
